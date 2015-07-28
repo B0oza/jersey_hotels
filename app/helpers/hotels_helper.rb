cc45933d1b0e4164a8fac00079984a961f9f2ff4
ben whitley <ben.whitley@hotmail.co.uk>
2015-07-28 18:41:14 +0100

    Fixed the stars

--- app/helpers/hotels_helper.rb
+++ app/helpers/hotels_helper.rb
@@ -1,2 +1,13 @@
 module HotelsHelper
+
+	def score_into_stars(score, total=5)
+	  ary = []
+	  score.times do |s|
+	    ary << (image_tag "star.svg")
+	  end
+	  (total - score).times do |s|
+	    ary << (image_tag "star-greyed-out.svg")
+	  end
+	  ary.join.html_safe
+	end
 end
